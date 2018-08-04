b996cda77873010364348fe9256f422fe5aee313
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2018-08-04 13:12:40 +0800

    Added custom Toolbar and colors

--- app/src/main/java/com/jjep/classes/MainActivity.kt
+++ app/src/main/java/com/jjep/classes/MainActivity.kt
@@ -2,11 +2,18 @@ package com.jjep.classes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.main_activity_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = getString(R.string.my_classes_title)
     }
 }
